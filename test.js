a80276aa67e97efbb5a86d517a653d81be25d2de
duyndgch18004 <70929901+duyndgch18004@users.noreply.github.com>
2020-09-10 11:51:40 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+    var i;
+    function mutiple9(){
+    for(i=1;i<=9;i++){
+    
+        console.log(`9 * ${i} = ${9*i}`);
+    }
+}
+mutiple9();
+var person = {
+    name: 'Duy Dep Trai',
+    age : 21
+    
+}
+function showPerson(p){
+    console.log(`Name : ${p.name} and Age : ${p.age}`)
+}
+showPerson(person);
